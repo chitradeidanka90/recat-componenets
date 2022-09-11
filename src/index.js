d63abe83dee4d78ac7d36d316d7ce48c7dff0f16
Chitra Daidanka <chitra@toddleapp.com>
2022-09-11 14:26:28 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom/client";
+import CommentDetails from "./CommentDetails";
+import ApprovalCard from "./ApprovalCard";
+// import './style/App.css'
+const { faker } = require('@faker-js/faker');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const el= document.getElementById('root')
+const root=ReactDom.createRoot(el)
+
+const App =()=>{
+ return (
+    <div className="ui container comments">
+      <ApprovalCard>
+      <CommentDetails author="sem" timeago="Today 6:00PM" content='i like this post' avatar={faker.image.avatar()} />
+      </ApprovalCard>
+
+      <ApprovalCard>
+      <CommentDetails author="alex"  timeago="Today 8:00PM" content="nice blog" avatar={faker.image.avatar()} />
+      </ApprovalCard>
+
+      <ApprovalCard>
+      <CommentDetails author="chitra" timeago="Today 9:00PM"  content="i like this subject" avatar={faker.image.avatar()} />
+      </ApprovalCard>
+
+       
+       
+       
+    </div>
+ );
+}
+
+root.render(<App />)
